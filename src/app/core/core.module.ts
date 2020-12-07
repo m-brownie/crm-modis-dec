1d0198cdaa2c504afd1b5805dfe07c6209994636
Matthieu <matthieu.bournat03@gmail.com>
2020-12-07 17:00:50 +0100

    Ajout de bootstrap

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { IconsModule } from '../icons/icons.module';
 import { LoginModule } from '../login/login.module';
 import { TextsModule } from '../texts/texts.module';
 import { UiModule } from '../ui/ui.module';
-
 
 
 @NgModule({
@@ -16,7 +16,8 @@ import { UiModule } from '../ui/ui.module';
     LoginModule,
     UiModule,
     TextsModule,
-    IconsModule
+    IconsModule,
+    NgbModule
   ]
 })
 export class CoreModule { }
