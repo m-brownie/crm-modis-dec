10ec29382857aa863d16fe94bf8a11b2fec1331f
Matthieu <matthieu.bournat03@gmail.com>
2020-12-08 10:01:49 +0100

    Ajout d'icones depuis Fontawesome

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconCloseComponent } from './icon-close/icon-close.component';
+import { IconDeleteComponent } from './icon-delete/icon-delete.component';
+import { IconEditComponent } from './icon-edit/icon-edit.component';
+import { IconNavComponent } from './icon-nav/icon-nav.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [IconNavComponent, IconCloseComponent, IconDeleteComponent, IconEditComponent],
   imports: [
     CommonModule,
     FontAwesomeModule
-  ]
+  ],
+  exports: [IconNavComponent, IconCloseComponent, IconDeleteComponent, IconEditComponent]
 })
 export class IconsModule { }
